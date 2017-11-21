5f730edaa41409a0d47db8592c89fb8f3664a9f9
PengPeng1121 <384607078@qq.com>
2017-11-21 16:38:40 +0800

    11.21修改版本  修改首页展示图片问题

--- src/main/java/com/free4lab/babycheckup/action/AccountAction.java
+++ src/main/java/com/free4lab/babycheckup/action/AccountAction.java
@@ -102,6 +102,14 @@ public class AccountAction {
         }
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public HttpServletRequest getReq() {
         return req;
     }
