77c1b411b7d37ca5f037658c114732517cacd600
PengPeng1121 <384607078@qq.com>
2017-12-01 20:58:29 +0800

    2期

--- src/main/java/com/free4lab/babycheckup/action/HomeAction.java
+++ src/main/java/com/free4lab/babycheckup/action/HomeAction.java
@@ -10,12 +10,16 @@ public class HomeAction {
     private long numberOfBaby;
     private long numberOfResult0_6;
     private long numberOfResult3_6;
+    private long numberOfResult0_2;
+    private long numberOfResult50;
 
     public String execute() throws Exception {
         int hoid = (Integer) ActionContext.getContext().getSession().get("hoid");
         numberOfBaby = BabyManager.countBabyByHoid(hoid);
         numberOfResult0_6 = ResultManager.countResultByHosIdAndTestId(hoid,1);
         numberOfResult3_6 = ResultManager3_6.countResultByHosIdAndTestId(hoid,2);
+        numberOfResult0_2 = ResultManager.countResultByHosIdAndTestId(hoid,16);
+        numberOfResult50 = ResultManager3_6.countResultByHosIdAndTestId(hoid,17);
         return "success";
     }
 
@@ -40,5 +44,21 @@ public class HomeAction {
 
     public void setNumberOfResult3_6(long numberOfResult6_12) {
         this.numberOfResult3_6 = numberOfResult6_12;
+    }
+
+    public long getNumberOfResult0_2() {
+        return numberOfResult0_2;
+    }
+
+    public void setNumberOfResult0_2(long numberOfResult0_2) {
+        this.numberOfResult0_2 = numberOfResult0_2;
+    }
+
+    public long getNumberOfResult50() {
+        return numberOfResult50;
+    }
+
+    public void setNumberOfResult50(long numberOfResult50) {
+        this.numberOfResult50 = numberOfResult50;
     }
 }
