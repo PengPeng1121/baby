5f0fc51e1c3321f45de7c822d6d044d05a371a94
pengpeng <384607078@qq.com>
2020-11-01 21:20:47 +0800

    all test

---
+++ src/main/java/com/free4lab/babycheckup/vo/AllTestResultVo.java
@@ -0,0 +1,349 @@
+package com.free4lab.babycheckup.vo;
+
+import com.free4lab.babycheckup.model.*;
+
+//儿童成长发育基础信息
+public class AllTestResultVo {
+
+//    private Result result;
+
+//    private Result0_2 result0_2;
+//
+//    private Result0_3 result0_3;
+//
+//    private Result3_6 result3_6;
+//
+//    private Result50 result50;
+//
+//    private Result132 result132;
+
+    private Result2016 result2016;
+
+    private ResultADHD resultADHD;
+
+    private ResultAllergy resultAllergy;
+
+    private ResultAttention resultAttention;
+
+    private ResultBasic1 resultBasic1;
+
+    private ResultBasic2 resultBasic2;
+
+    private ResultBless resultBless;
+
+    private ResultBlood resultBlood;
+
+    private ResultBloodType resultBloodType;
+
+    private ResultBMD resultBMD;
+
+    private ResultBone resultBone;
+
+    private ResultCognize resultCognize;
+
+    private ResultDDST resultDDST;
+
+    private ResultEar resultEar;
+
+    private ResultEye resultEye;
+
+    private ResultFastAllergy resultFastAllergy;
+
+    private ResultFeed resultFeed;
+
+    private ResultFeel resultFeel;
+
+    private ResultFeel2020 resultFeel2020;
+
+    private ResultFoodAllergy resultFoodAllergy;
+
+    private ResultGroup resultGroup;
+
+    private ResultGroup2020 resultGroup2020;
+
+    private ResultHbs resultHbs;
+
+    private ResultMicro resultMicro;
+
+    private ResultNei resultNei;
+
+    private ResultPhysical resultPhysical;
+
+    private ResultPlan resultPlan;
+
+    private ResultQiZhi resultQiZhi;
+
+    private ResultQiZhi2020 resultQiZhi2020;
+
+    private ResultRear resultRear;
+
+    private ResultSummary resultSummary;
+
+    private ResultTooth resultTooth;
+
+    private ResultUrine resultUrine;
+
+    public Result2016 getResult2016() {
+        return result2016;
+    }
+
+    public void setResult2016(Result2016 result2016) {
+        this.result2016 = result2016;
+    }
+
+    public ResultADHD getResultADHD() {
+        return resultADHD;
+    }
+
+    public void setResultADHD(ResultADHD resultADHD) {
+        this.resultADHD = resultADHD;
+    }
+
+    public ResultAllergy getResultAllergy() {
+        return resultAllergy;
+    }
+
+    public void setResultAllergy(ResultAllergy resultAllergy) {
+        this.resultAllergy = resultAllergy;
+    }
+
+    public ResultAttention getResultAttention() {
+        return resultAttention;
+    }
+
+    public void setResultAttention(ResultAttention resultAttention) {
+        this.resultAttention = resultAttention;
+    }
+
+    public ResultBasic1 getResultBasic1() {
+        return resultBasic1;
+    }
+
+    public void setResultBasic1(ResultBasic1 resultBasic1) {
+        this.resultBasic1 = resultBasic1;
+    }
+
+    public ResultBasic2 getResultBasic2() {
+        return resultBasic2;
+    }
+
+    public void setResultBasic2(ResultBasic2 resultBasic2) {
+        this.resultBasic2 = resultBasic2;
+    }
+
+    public ResultBless getResultBless() {
+        return resultBless;
+    }
+
+    public void setResultBless(ResultBless resultBless) {
+        this.resultBless = resultBless;
+    }
+
+    public ResultBlood getResultBlood() {
+        return resultBlood;
+    }
+
+    public void setResultBlood(ResultBlood resultBlood) {
+        this.resultBlood = resultBlood;
+    }
+
+    public ResultBloodType getResultBloodType() {
+        return resultBloodType;
+    }
+
+    public void setResultBloodType(ResultBloodType resultBloodType) {
+        this.resultBloodType = resultBloodType;
+    }
+
+    public ResultBMD getResultBMD() {
+        return resultBMD;
+    }
+
+    public void setResultBMD(ResultBMD resultBMD) {
+        this.resultBMD = resultBMD;
+    }
+
+    public ResultBone getResultBone() {
+        return resultBone;
+    }
+
+    public void setResultBone(ResultBone resultBone) {
+        this.resultBone = resultBone;
+    }
+
+    public ResultCognize getResultCognize() {
+        return resultCognize;
+    }
+
+    public void setResultCognize(ResultCognize resultCognize) {
+        this.resultCognize = resultCognize;
+    }
+
+    public ResultDDST getResultDDST() {
+        return resultDDST;
+    }
+
+    public void setResultDDST(ResultDDST resultDDST) {
+        this.resultDDST = resultDDST;
+    }
+
+    public ResultEar getResultEar() {
+        return resultEar;
+    }
+
+    public void setResultEar(ResultEar resultEar) {
+        this.resultEar = resultEar;
+    }
+
+    public ResultEye getResultEye() {
+        return resultEye;
+    }
+
+    public void setResultEye(ResultEye resultEye) {
+        this.resultEye = resultEye;
+    }
+
+    public ResultFastAllergy getResultFastAllergy() {
+        return resultFastAllergy;
+    }
+
+    public void setResultFastAllergy(ResultFastAllergy resultFastAllergy) {
+        this.resultFastAllergy = resultFastAllergy;
+    }
+
+    public ResultFeed getResultFeed() {
+        return resultFeed;
+    }
+
+    public void setResultFeed(ResultFeed resultFeed) {
+        this.resultFeed = resultFeed;
+    }
+
+    public ResultFeel getResultFeel() {
+        return resultFeel;
+    }
+
+    public void setResultFeel(ResultFeel resultFeel) {
+        this.resultFeel = resultFeel;
+    }
+
+    public ResultFeel2020 getResultFeel2020() {
+        return resultFeel2020;
+    }
+
+    public void setResultFeel2020(ResultFeel2020 resultFeel2020) {
+        this.resultFeel2020 = resultFeel2020;
+    }
+
+    public ResultFoodAllergy getResultFoodAllergy() {
+        return resultFoodAllergy;
+    }
+
+    public void setResultFoodAllergy(ResultFoodAllergy resultFoodAllergy) {
+        this.resultFoodAllergy = resultFoodAllergy;
+    }
+
+    public ResultGroup getResultGroup() {
+        return resultGroup;
+    }
+
+    public void setResultGroup(ResultGroup resultGroup) {
+        this.resultGroup = resultGroup;
+    }
+
+    public ResultGroup2020 getResultGroup2020() {
+        return resultGroup2020;
+    }
+
+    public void setResultGroup2020(ResultGroup2020 resultGroup2020) {
+        this.resultGroup2020 = resultGroup2020;
+    }
+
+    public ResultHbs getResultHbs() {
+        return resultHbs;
+    }
+
+    public void setResultHbs(ResultHbs resultHbs) {
+        this.resultHbs = resultHbs;
+    }
+
+    public ResultMicro getResultMicro() {
+        return resultMicro;
+    }
+
+    public void setResultMicro(ResultMicro resultMicro) {
+        this.resultMicro = resultMicro;
+    }
+
+    public ResultNei getResultNei() {
+        return resultNei;
+    }
+
+    public void setResultNei(ResultNei resultNei) {
+        this.resultNei = resultNei;
+    }
+
+    public ResultPhysical getResultPhysical() {
+        return resultPhysical;
+    }
+
+    public void setResultPhysical(ResultPhysical resultPhysical) {
+        this.resultPhysical = resultPhysical;
+    }
+
+    public ResultPlan getResultPlan() {
+        return resultPlan;
+    }
+
+    public void setResultPlan(ResultPlan resultPlan) {
+        this.resultPlan = resultPlan;
+    }
+
+    public ResultQiZhi getResultQiZhi() {
+        return resultQiZhi;
+    }
+
+    public void setResultQiZhi(ResultQiZhi resultQiZhi) {
+        this.resultQiZhi = resultQiZhi;
+    }
+
+    public ResultQiZhi2020 getResultQiZhi2020() {
+        return resultQiZhi2020;
+    }
+
+    public void setResultQiZhi2020(ResultQiZhi2020 resultQiZhi2020) {
+        this.resultQiZhi2020 = resultQiZhi2020;
+    }
+
+    public ResultRear getResultRear() {
+        return resultRear;
+    }
+
+    public void setResultRear(ResultRear resultRear) {
+        this.resultRear = resultRear;
+    }
+
+    public ResultSummary getResultSummary() {
+        return resultSummary;
+    }
+
+    public void setResultSummary(ResultSummary resultSummary) {
+        this.resultSummary = resultSummary;
+    }
+
+    public ResultTooth getResultTooth() {
+        return resultTooth;
+    }
+
+    public void setResultTooth(ResultTooth resultTooth) {
+        this.resultTooth = resultTooth;
+    }
+
+    public ResultUrine getResultUrine() {
+        return resultUrine;
+    }
+
+    public void setResultUrine(ResultUrine resultUrine) {
+        this.resultUrine = resultUrine;
+    }
+}
